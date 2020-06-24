f8bdb9a5422acf8929a487367c11b8a2b39e2320
“dinadeljanin” <ddeljanin5@icloud.com>
2020-06-23 22:41:47 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,41 +1,75 @@
 const taskForm = document.querySelector('#create-task-form');
 const taskList = document.querySelector("#tasks");
-const taskDropdown = document.querySelector("#new-task-priority")
+const taskDropdown = document.querySelector("#new-task-priority");
+const taskSort = document.querySelector("#sortBy");
 
 
 taskForm.addEventListener("submit", function() {
   event.preventDefault();
 
+  const taskName = document.querySelector("#new-user").value
   const taskStr = document.querySelector("#new-task-description").value;
+  const taskDuration = document.querySelector("#new-duration").value;
+  const taskDueDate = document.querySelector("#new-due-date").value;
   const taskPriority = document.querySelector("#new-task-priority").value;
   let taskColor;
 
   // brute foooooooooooorce
-  if (taskPriority === "high") {
+  // or you can also brute force adding classes, yes
+  if (taskPriority === "high"){
     taskColor = "red";
-  } else if (taskPriority === "medium") {
+  } else if (taskPriority === "medium"){
     taskColor = "orange";
   } else if (taskPriority == "low"){
     taskColor = "green";
-  } else {
-    // add something in here that tells you to try again
   }
 
-  const taskItem = document.createElement("li");
+  const obj = {
+      name: taskName,
+      task: taskStr,
+      duration: taskDuration,
+      dueDate: taskDueDate,
+      priority: taskPriority,
+      priorityColor: taskColor
+    };
+
+  renderRows(obj);
+
+});
+
+function renderRows(obj) {
+  const taskItem = document.createElement("tr");
   taskItem.innerHTML = `
-    <p>${taskStr}</p>
-    <p style="color: ${taskColor};" class="priority">${taskPriority.toUpperCase()}</p>
-    <button class="delBtn">delete</button>
+    <td>${obj.name}</td>
+    <td>${obj.task}</td>
+    <td>${obj.duration}</td>
+    <td>${obj.dueDate}</td>
+    <td style="background-color: ${obj.priorityColor};" class="priority">${obj.priority}</td>
+    <td>
+      <button class="delBtn">delete</button>
+      <button class="editBtn">edit</button>
+    </td>
   `;
 
   // console.log(task);
   taskList.appendChild(taskItem);
   taskForm.reset();
-});
+}
 
 taskList.addEventListener("click", function(e) {
   console.log(e.target.className);
   if (e.target.className === 'delBtn') {
     e.target.parentElement.remove();
+  } else if (e.target.className === 'editBtn') {
+    // e.target.parentElement
+    // do you need a new function?
   }
+})
+
+// Sorting Pseudocode
+// 1. get ALLLLLLLLLL the TR elements created
+// 2. ?????????
+// 3. sort by priority heh'
+taskSort.addEventListener("change", function(e) {
+  // console.log("hold my coffee, im going in")
 })
